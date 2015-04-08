d269dbd514ffae8aa513b7e4cc6cddc3ad39067e
Ahmad Hamadeh <ahmad.hamadeh@accenture.com>
2015-04-08 12:23:30 +0200

    0.4

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -4,8 +4,8 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "ahmadhamadeh"
-client_key               "#{current_dir}/ahmadhamadeh.pem"
+client_key               "#{current_dir}/ahmadhamadeh"
 validation_client_name   "ahad-validator"
-validation_key           "#{current_dir}/ahad-validator.pem"
+validation_key           "#{current_dir}/ahad-validator"
 chef_server_url          "https://api.opscode.com/organizations/ahad"
 cookbook_path            ["#{current_dir}/../cookbooks"]
